e48d6b1cef5325f76dd1954f5689c25d93ceacb9
TeoContreras <teo2008contrerasuniversidad@gmail.com>
2026-08-18 15:15:16 -0300

    ejercicio 4

--- unidad 1/ejercicio1.java
+++ unidad 1/ejercicio1.java
@@ -1,5 +1,4 @@
 public class ejercicio1 {
-
     public static void main(String[] args){
         CuentaBancaria c1= new CuentaBancaria("Teo", "001", 100.00);
         CuentaBancaria c2= new CuentaBancaria("Luis", "002", 200.00);
@@ -9,32 +8,26 @@ public class ejercicio1 {
         c1.retirar(200.);
         c2.retirar(600.0);
         c2.depositar(300.);
-
         System.out.println("\n=== Estado Final ===\n" + c1 + "\n---\n" + c2);
-
     }
 }
 
 class CuentaBancaria{
     private final String titular, numeroCuenta;
     private double saldo;
-
     public CuentaBancaria(String titular, String numeroCuenta, double saldo){
         this.titular = titular;
         this.numeroCuenta= numeroCuenta;
         this.saldo = saldo;
     }
-
     public void depositar(double monto){
         if (monto > 0) saldo += monto;
         else System.out.println("Fondos insuficientes o monto invalido. ");
     }
-
     public void retirar(double monto){
         if (monto>0 && monto <= saldo) saldo -= monto;
         else System.out.println("Fondos insuficientes o monto invalido.");
     }
-
     @Override
     public String toString(){
         return "Titular: " + titular + " | Cuenta: " + numeroCuenta + " | Saldo: $" + saldo;
--- unidad 1/ejercicio3.java
+++ unidad 1/ejercicio3.java
@@ -1,2 +1,44 @@
 public class ejercicio3 {
+    public static void main(String[] args){
+    Vehiculo v1 = new Vehiculo("Fiat", "Cronno 2025", 0);
+    int[] Velocidad ={10,20,30,-10,-35,-20, 10, 20, -20};
+    for (int v : Velocidad){
+        System.out.println("---------");
+        v1.Estado(v, v1);
+    }
+    }
+}
+class Vehiculo{
+    private String marca;
+    private String modelo;
+    int velocidadActual;
+    public Vehiculo(String marca, String modelo, int velocidadActual){
+        this.marca=marca;
+        this.modelo=modelo;
+        this.velocidadActual=velocidadActual;
+    }
+    public double masVel(int Velocidad){
+        return this.velocidadActual=velocidadActual+Velocidad;
+    }
+    public int menosVel(int Velocidad) {
+        int cantidadRestar = Math.abs(Velocidad);
+        if (velocidadActual < cantidadRestar) {
+            System.out.println("Velocidad agregada: "+Velocidad+"\nVelocidad erronea. Velocidad actual: 0");
+            this.velocidadActual = 0;
+        } else {
+            this.velocidadActual -= cantidadRestar;
+        }
+        return this.velocidadActual;
+    }
+    public void Estado(int v, Vehiculo v1){
+        if (v>0){
+            v1.masVel(v);
+            System.out.println("Velocidad acelerada: "+v);
+        }
+        else {
+            v1.menosVel(v);
+            System.out.println("Velocidad disminuida: "+v);
+        }
+        System.out.println("\nVelocidad actual: "+velocidadActual);
+    }
 }
